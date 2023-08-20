9808f1ec87e447f348b8bf653d28af70d6e32ae9
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-08-20 10:10:20 +0100

    Formatting  using Prettier

--- src/requests/getImages.js
+++ src/requests/getImages.js
@@ -8,8 +8,10 @@ const getImages = async (query) => {
       .get(`https://images-api.nasa.gov/search?q=${query}`)
       .then((response) => {
         const imageResults = response.data.collection.items;
-        const parsedImages = imageResults.filter(data => data.data[0].media_type === 'image');
-        const images = parsedImages.map(image => image.links[0].href);
+        const parsedImages = imageResults.filter(
+          (imageResults) => imageResults.data[0].media_type === "image"
+        );
+        const images = parsedImages.map((image) => image.links[0].href);
         return images;
       })
       .catch((error) => {
