69118e96a6c858518a1917332bd438eb45f22599
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-08-20 20:26:06 +0100

    Improved testing methods introduced for Search.test.js

--- src/__tests__/Search.test.js
+++ src/__tests__/Search.test.js
@@ -1,10 +1,32 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import Search from "../components/Search";
 
 describe("Search", () => {
   it("Renders Correctly", () => {
     const { asFragment } = render(<Search />);
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("Updates input value when typing", () => {
+    render(<Search />);
+    const inputElement = screen.getByPlaceholderText("Enter keywords...");
+
+    fireEvent.change(inputElement, {target: { value: "space" }} );
+    expect(inputElement.value).toBe("space");
+  });
+
+  it("Calls setSearchResults when form is submitted", async () => {
+    const setSearchResultsMock = jest.fn();
+    render(<Search setSearchResults={setSearchResultsMock} />);
+    const inputElement = screen.getByPlaceholderText("Enter keywords...");
+    const submitButton = screen.getByText("Go!");
+
+    fireEvent.change(inputElement, { target: { value: "moon" } });
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(setSearchResultsMock).toHaveBeenCalledWith(expect.any(Array));
+    });
   });
 });
--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -3,13 +3,13 @@ import { render } from "@testing-library/react";
 import SearchResults from "../components/SearchResults";
 
 describe("SearchResults", () => {
-  it("Renders Correctly", () => {
-    const mockResults = [
-      "https://images-assets.nasa.gov/image/PIA12235/PIA12235~thumb.jpg",
-      "https://images-assets.nasa.gov/image/PIA13517/PIA13517~thumb.jpg",
-      "https://images-assets.nasa.gov/image/PIA12228/PIA12228~thumb.jpg",
-    ];
+  const mockResults = [
+    "https://images-assets.nasa.gov/image/PIA12235/PIA12235~thumb.jpg",
+    "https://images-assets.nasa.gov/image/PIA13517/PIA13517~thumb.jpg",
+    "https://images-assets.nasa.gov/image/PIA12228/PIA12228~thumb.jpg",
+  ];
 
+  it("Renders Correctly", () => {
     const { asFragment } = render(<SearchResults results={mockResults} />);
     expect(asFragment()).toMatchSnapshot();
   });
