d103fcce0f2ece902621747814e5b4f69b23ca0e
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-08-20 17:13:23 +0100

    Fixed SearchResults testing bug

--- src/__tests__/SearchResults.test.js
+++ src/__tests__/SearchResults.test.js
@@ -3,8 +3,14 @@ import { render } from "@testing-library/react";
 import SearchResults from "../components/SearchResults";
 
 describe("SearchResults", () => {
-  it("Renders and matches Snapshot", () => {
-    const { asFragment } = render(<SearchResults results="" />);
-    expect(asFragment).toMatchSnapshot();
+  it("Renders Correctly", () => {
+    const mockResults = [
+      "https://images-assets.nasa.gov/image/PIA12235/PIA12235~thumb.jpg",
+      "https://images-assets.nasa.gov/image/PIA13517/PIA13517~thumb.jpg",
+      "https://images-assets.nasa.gov/image/PIA12228/PIA12228~thumb.jpg",
+    ];
+
+    const { asFragment } = render(<SearchResults results={mockResults} />);
+    expect(asFragment()).toMatchSnapshot();
   });
 });
